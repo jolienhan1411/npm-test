ff06a12f35c544d8b7e63c868954cf64d9b0357f
jolienhan1411 <34261029+jolienhan1411@users.noreply.github.com>
2018-04-19 16:09:46 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "nhantest",
+  "version": "1.3.4",
+  "description": "this is a testing package",
+  "main": "helloworld.js",
+  "scripts": {
+    "test": "test"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git@github.com/jolienhan1411/npm-test.git"
+  },
+  "keywords": [
+    "don't",
+    "know"
+  ],
+  "author": "nhanvo",
+  "license": "UNLICENSED",
+  "dependencies": {}
+}
